d76e6e630eb14c46c8326766b851013c21ff04c2
Cabin Zhu <cabin_zhu@foxmail.com>
2024-04-28 17:40:17 +0000

    [New] Added `num_images_per_prompt` for inference.py

--- inference.py
+++ inference.py
@@ -14,13 +14,14 @@ cmd_parser.add_argument('-p', '--prompt_postfixes',
 # cmd_parser.add_argument('-i', '--prompt_img_type', type=str, default='photo')
 cmd_parser.add_argument('-l', '--loop_id', type=int, default=0) 
 cmd_parser.add_argument('-o', '--output_dir', type=str, default='./out/inference_results') 
+cmd_parser.add_argument('-n', '--num_images_per_prompt', type=int, default=5) 
 cmd_args = cmd_parser.parse_args()
 
 args = config2args(cmd_args.config_file)
 
 
 # Set up output directory
-output_dir = os.path.join(cmd_args.output_dir, args.character_name)
+output_dir = os.path.join(cmd_args.output_dir, args.character_name, f"loop={cmd_args.loop_id}")
 os.makedirs(output_dir, exist_ok=True)
 
 # Load models
@@ -32,10 +33,18 @@ pipe.load_lora_weights(os.path.join(model_path, f"checkpoint-{args.checkpointing
 # Infer
 for prompt_postfix in cmd_args.prompt_postfixes:
     img_filename_postfix = prompt_postfix.replace(" ", "_")
-    img_path = os.path.join(output_dir, f"{args.character_name}_{img_filename_postfix}.png")
     
-    # prompt = f"A photo of {args.placeholder_token} {prompt_postfix}."
-    prompt = f"{args.validation_prompt} {prompt_postfix}." # "A photo of <placeholder> ..."
-    image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
-
-    image.save(img_path)
+    prompt = f"A photo of {args.placeholder_token} {prompt_postfix}."
+    # prompt = f"{args.validation_prompt} {prompt_postfix}." # "A photo of <placeholder> ..."
+    
+    
+    # image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
+    imgs = pipe(prompt, 
+                num_inference_steps=35, 
+                guidance_scale=7.5, 
+                num_images_per_prompt=cmd_args.num_images_per_prompt
+            ).images
+    for sampling_id, img in enumerate(imgs):
+        img_path = os.path.join(
+            output_dir, f"{args.character_name}_{img_filename_postfix}_{sampling_id}.png")
+        img.save(img_path)
